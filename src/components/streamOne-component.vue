040e89cd222122744a5cc2e34a8badbcf27a803f
J2A2B <ja.andreani@gmail.com>
2017-10-02 14:49:31 +0200

    commit

---
+++ src/components/streamOne-component.vue
@@ -0,0 +1,25 @@
+<template>
+  <twitch-player :channel=channel>
+  </twitch-player>
+</template>
+
+<script>
+  // import Hello from './Hello.vue'
+  import VueTwitchPlayer from 'vue-twitch-player'
+export default {
+    components: {
+      'twitch-player': VueTwitchPlayer
+    },
+    data () {
+      return {
+        channel: this.channel
+      }
+    }
+    // methods: {
+    //   updateStream: function (updatedStream) {
+    //     this.channel = updatedStream
+    //   }
+    // }
+}
+// this.$Hello.selectStream('coucou')
+</script>
